d2fc0b8dde54df0ba96fd74965c1168955c466ad
alain17-web <web2020.alain@gmail.com>
2024-04-26 13:34:20 +0200

    manage project

---
+++ restaurant-manager-ui/src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+// @flow
+
+export const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    );
+};
---
+++ restaurant-manager-ui/src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import {Navbar} from "../../components/navbar/Navbar.tsx";
+
+export const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Home</h1>
+        </div>
+    );
+};
