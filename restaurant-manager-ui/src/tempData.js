42b1eb829ffbeb65b519c0273d9d1851ececa805
alain17-web <web2020.alain@gmail.com>
2024-05-04 15:49:39 +0200

    ADD tempData file

---
+++ restaurant-manager-ui/src/tempData.js
@@ -0,0 +1,232 @@
+export const dishes = [
+    {
+        id:1,
+        name:"Msabaha",
+        desc:"Une des variantes de houmous les plus appréciées au moyen orient! Elle se compose de purée de pois chiches, crème de sésame, jus de citron, ail concassé, persil, gains de pois chiche entiers.",
+        img:"./img/Msabaha.jpg",
+        price:6.5,
+        cat:"Mezze"
+    },
+    {
+        id:2,
+        name:"Shoumar",
+        desc:"Salade de fenouil, aneth, jeunes oignons, raisins secs et airelles",
+        img:"./img/shoumar.jpg",
+        price:6.5,
+        cat:"Mezze"
+    },
+    {
+        id:3,
+        name:"Houmous",
+        desc:"Purée de pois chiches, crème de sésame, jus de citron, épices du pays",
+        img:"./img/houmous.jpg",
+        price:6,
+        cat:"Mezze"
+    },
+    {
+        id:4,
+        name:"Fatouche",
+        desc:"Salade composée, pétales de pain grillé, sirop de grenade.",
+        img:"./img/Msabaha.jpg",
+        price:5.5,
+        cat:"Mezze"
+    },
+    {
+        id:5,
+        name:"Baba Ghanouj",
+        desc:"Grains de grenade, aubergines grillées, tomates, oignons, poivrons et persil plat, huile d’olive et épices du pays.",
+        img:"./img/baba_ghanouj.jpg",
+        price:6.5,
+        cat:"Mezze"
+    },
+    {
+        id:6,
+        name:"Moussaka Palestinienne",
+        desc:"Aubergines grillées, pois chiches, oignons et sauce tomate, le tout cuisiné au four.",
+        img:"./img/mousaka.jpg",
+        price:6.5,
+        cat:"Mezze"
+    },
+    {
+        id:7,
+        name:"Moutabal",
+        desc:"Aubergines grillées façon caviar, jus de citron et crème de sésame, épices du pays.",
+        img:"./img/moutabal.jpg",
+        price:5.5,
+        cat:"Mezze"
+    },
+    {
+        id:8,
+        name:"Taboule",
+        desc:"Persil plat, tomates, oignons, jus de citron, menthe fraîche, semoule, huile d’olive et savoir-faire du Chef.",
+        img:"./img/taboule.jpg",
+        price:6,
+        cat:"Mezze"
+    },{
+        id:9,
+        name:"Batata Harrara",
+        desc:"Dés de pommes de terre marinés aux épices maison, ail, coriandre & harissa.",
+        img:"./img/batata.jpg",
+        price:6,
+        cat:"Mezze"
+    },
+    {
+        id:10,
+        name:"Falafel d'Akko",
+        desc:"Croquette végétarienne à base de pois chiches moulu, ail, oignons, coriandre, épices du pays.",
+        img:"./img/falafel.jpg",
+        price:1.3,
+        cat:"Mezze"
+    },
+    {
+        id:11,
+        name:"Shorbat Adas",
+        desc:"Potage palestinien aux lentilles de corail, oignons et jus de citron.",
+        img:"./img/shorbat.jpg",
+        price:6,
+        cat:"Mezze"
+    },
+    {
+        id:12,
+        name:"Aji",
+        desc:"Omelette végétarienne aux légumes, persil plat, oignons, tomates, courgettes, œufs fermiers.",
+        img:"./img/aji.jpg",
+        price:4.5,
+        cat:"Mezze"
+    },
+    {
+        id:13,
+        name:"Assiette Chich Taouk",
+        desc:"2 Brochettes de poulet mariné Les assiettes grillades sont servies avec des crudités, des batata harra (dés de pommes de terre marinés aux épices maison, ail, coriandre & harissa) ou des frites et une sauce au choix.",
+        img:"./img/chichtaouk.jpg",
+        price:16.5,
+        cat:"grill"
+    },
+    {
+        id:14,
+        name:"Assiette Kafta Galilée",
+        desc:"2 Brochettes de viande hachée, aromatisée de boeuf et d’agneau Les assiettes grillades sont servies avec des crudités, des batata harra (dés de pommes de terre marinés aux épices maison, ail, coriandre & harissa) ou des frites et une sauce au choix",
+        img:"./img/kafta.jpg",
+        price:16.5,
+        cat:"grill"
+    },
+    {
+        id:15,
+        name:"Assiette Lahme Mechwi",
+        desc:"2 Brochettes d’agneau mariné Les assiettes grillades sont servies avec des crudités, des batata harra (dés de pommes de terre marinés aux épices maison, ail, coriandre & harissa) ou des frites et une sauce au choix",
+        img:"./img/lahme.jpg",
+        price:16.5,
+        cat:"grill"
+    },
+    {
+        id:16,
+        name:"Assiette Artichauts",
+        desc:"3 Coeurs d'artichaut farcis à la viande hachée, houmous, taboulé, baba ghanou",
+        img:"./img/assiettes-artichauts.jpg",
+        price:17.5,
+        cat:"assiette froide"
+    },
+    {
+        id:17,
+        name:"Assiette Kebbeh d'Arraba",
+        desc:"3 Kebbehs, taboulé, baba ghanouj, houmous et sauce à l'ail (G; N; S)",
+        img:"./img/kebbeh.jpg",
+        price:16.5,
+        cat:"assiette froide"
+    },
+    {
+        id:18,
+        name:"Assiette Falafel d'Akko",
+        desc:"3 Falafels, houmous, taboulé, baba ghanouj et sauce tahina",
+        img:"./img/assiette-falafel.jpg",
+        price:14.5,
+        cat:"assiette froide"
+    },
+    {
+        id:19,
+        name:"Aish el saraya",
+        desc:"Dessert à la crème et aux pistache",
+        img:"./img/aish.jpg",
+        price:4.5,
+        cat:"dessert"
+    },
+    {
+        id:20,
+        name:"Baklava",
+        desc:"Pâte feuilletée, pistache, cajou et amande, sirop de miel à la fleur d’oranger",
+        img:"./img/baklava.jpg",
+        price:3.5,
+        cat:"dessert"
+    },
+    {
+        id:21,
+        name:"Namoura",
+        desc:"Gâteau de semoule, fleur d’oranger, amandes, sirop au miel maison",
+        img:"./img/namoura.jpg",
+        price:4.5,
+        cat:"dessert"
+    },
+]
+
+export const drinks = [
+    {
+        idDrink:1,
+        name:"Coca,Coca Zero,Sprite,Fanta,Ice tea",
+        price:1.5,
+        cat:"soft"
+    },
+    {
+        idDrink:2,
+        name:"Thé à la menthe fraîche",
+        price:2,
+        cat:"boisson chaude"
+    },
+    {
+        idDrink:3,
+        name:"Thé à la Camomille",
+        price:1.7,
+        cat:"boisson chaude"
+    },
+    {
+        idDrink:4,
+        name:"Café",
+        price:1.6,
+        cat:"boisson chaude"
+    },
+    {
+        idDrink:5,
+        name:"Expresso",
+        price:1.5,
+        cat:"boisson chaude"
+    },
+    {
+        idDrink:6,
+        name:"Château Kefraya - Cab Sauvignon-Syrah",
+        price:75,
+        cat:"vin"
+    },
+    {
+        idDrink:7,
+        name:"Ksara - Cab Sauvignon-Syrah",
+        price:36,
+        cat:"vin"
+    },
+    {
+        idDrink:8,
+        name:"Château St Thomas - Merlot-Syrah",
+        price:69,
+        cat:"vin"
+    },
+    {
+        idDrink:9,
+        name:"Taybeh Blonde",
+        price:4.5,
+        cat:"bière"
+    },
+    {
+        idDrink:10,
+        name:"Taybeh Blanche",
+        price:4.5,
+        cat:"bière"
+    },
+]
