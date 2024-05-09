53f3de48d60304f686626f26cd96212213f371c2
alain17-web <web2020.alain@gmail.com>
2024-05-09 21:46:17 +0200

    IMPROV MenuSidebar and Menu

--- restaurant-manager-ui/src/components/menuSidebar/MenuSidebar.tsx
+++ restaurant-manager-ui/src/components/menuSidebar/MenuSidebar.tsx
@@ -2,18 +2,27 @@ import {dishes} from '../../tempData.ts'
 import { Link } from "react-router-dom"
 import Drinks from "../drinks/Drinks.tsx";
 
-const MenuSidebar = () => {
+const MenuSidebar = ({onItemSelect,selectedItem}) => {
 
     const categories:string[] = []
     dishes.forEach((dish) => {
         if(!categories.includes(dish.cat))
         categories.push(dish.cat)
     })
+
+    const handleClick = (cat) => {
+        onItemSelect(cat)
+    }
+
     return (
         <main className={"w-[15%] h-auto flex flex-col ml-12"}>
             <h1 className={"text-2xl font-inter underline"}>Les plats</h1>
             {categories.map(cat => (
-                <Link className={"font-inter font-semibold italic text-[#013220] text-2xl mt-5"} key={cat}>
+                <Link
+                    className={selectedItem === cat ? "font-inter font-semibold italic text-amber-100 underline text-2xl mt-5" : "font-inter font-semibold italic text-[#013220] text-2xl mt-5"}
+                    key={cat}
+                    onClick={() =>handleClick(cat)}
+                >
                     {cat}
                 </Link>
             ))}
--- restaurant-manager-ui/src/pages/menu/Menu.tsx
+++ restaurant-manager-ui/src/pages/menu/Menu.tsx
@@ -1,9 +1,13 @@
 import Navbar from "../../components/navbar/Navbar.tsx";
 import {dishes} from '../../tempData.ts'
 import MenuSidebar from "../../components/menuSidebar/MenuSidebar.tsx";
+import {useState} from "react";
 
 
 export const Menu = () => {
+
+    const [selectedItem, setSelectedItem] = useState("Mezzes")
+
     return (
         <section className={"w-full h-screen bg-no-repeat bg-center bg-cover"}
                  style={{backgroundImage: `url('./img/scenery.jpg')`}}>
@@ -11,13 +15,13 @@ export const Menu = () => {
             <h1 className={"text-center text-5xl font-inter italic text-[#013220] mt-16"}>Bienvenue - مَرْحَباً - ברוך
                 הבא</h1>
            <section className={"w-full flex"}>
-               <MenuSidebar/>
+               <MenuSidebar onItemSelect={setSelectedItem} selectedItem={selectedItem}/>
             <main className={"w-[80%] mx-auto mt-12"}>
                 <ul className={"w-[60%] mx-auto"}>
-                    <h3 className={"font-inter italic text-2xl text-amber-50 underline mb-5"}>Les Mezzes</h3>
+                    <h3 className={"font-inter italic text-2xl text-amber-50 underline mb-5"}>Les {selectedItem}</h3>
                     {dishes.map((dish) => (
                         <div key={dish.id}>
-                            {dish.cat === "Mezzes" ? (
+                            {dish.cat === `${selectedItem}` ? (
                                 <li className={"list-none"}>
                                     <h3 className={"font-inter italic text-xl text-[#013220] font-bold"}>{dish.name} - {dish.price + "€"}</h3>
                                     <p className={"mb-4 text-lg text-[#013220] font-inter italic semibold"}>{dish.desc}</p>
