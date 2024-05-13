6db4558a788628cbccaf8bc4753b1cbbbfe224b3
alain17-web <web2020.alain@gmail.com>
2024-05-13 15:59:06 +0200

    links in fr

--- restaurant-manager-ui/src/App.tsx
+++ restaurant-manager-ui/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
             element: <Menu />
         },
         {
-            path: "/about",
+            path: "/reservation-infos",
             element: <About />
         },
 
--- restaurant-manager-ui/src/components/bookButton/BookButton.tsx
+++ restaurant-manager-ui/src/components/bookButton/BookButton.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 
 const BookButton = () => {
     return (
-        <Link to={"/about"}>
+        <Link to={"/reservation-infos"}>
             <button className={"text-xl text-[#013220] hover:text-amber-50 hover:bg-[#013220] font-inter italic outline outline-2 outline-offset-1 outline-[#013220] px-4 py-2 rounded-md"}
             >
                 Réserver
