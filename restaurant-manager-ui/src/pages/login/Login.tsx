46aefb189e478a5669b05113cd8ed74502f1348a
alain17-web <web2020.alain@gmail.com>
2024-05-16 14:17:13 +0200

    ADD Login UI

--- restaurant-manager-ui/src/pages/login/Login.tsx
+++ restaurant-manager-ui/src/pages/login/Login.tsx
@@ -1,9 +1,30 @@
+import Logo from "../../components/logo/Logo.tsx";
+import {Link} from "react-router-dom"
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <section
+            className={"w-full h-screen overflow-hidden bg-amber-50"}>
+            <header className={"pl-10 pt-10"}>
+                <Link to={"/"} className={"flex justify-start items-center"}>
+                    <Logo/>
+                    <p className={"font-inter text-center text-[#013220] hover:text-[#6B8E23] hover:underline text-base italic mt-12"}>Accueil</p>
+                </Link>
+            <h3 className={"font-inter text-5xl text-[#013220] text-center font-medium italic mt-12"}>La Branche d'Olivier</h3>
+            </header>
+            <main
+                className={"w-[40%] h-[40vh] mx-auto mt-[20vh] bg-[url('./img/logo.png')] bg-top bg-no-repeat bg-cover rounded-full"}
+            >
+                <form
+                    className={"w-full flex flex-col items-center justify-around gap-8 pt-20"}
+                    noValidate
+                >
+                    <input type={"text"} placeholder={"Nom d'utilisateur"} className={"h-10 bg-white  placeholder:pl-2 rounded-md pl-2"}/>
+                    <input type={"password"} placeholder={"mot de passe"} className={"h-10 bg-white  placeholder:pl-2 rounded-md pl-2"} />
+                    <button type={"submit"} className={"h-10 bg-[#663399] hover:bg-amber-800 text-white px-16 cursor-pointer"}>Connexion</button>
+                </form>
+            </main>
+        </section>
     );
 };
 export default Login;
