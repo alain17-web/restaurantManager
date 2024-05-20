77aa4d8f89113de43eb29d77ba6110aff763ec04
alain17-web <web2020.alain@gmail.com>
2024-05-20 12:54:28 +0200

    FEATURE getRandomItems to NewOrder

--- restaurant-manager-ui/src/components/newOrder/NewOrder.tsx
+++ restaurant-manager-ui/src/components/newOrder/NewOrder.tsx
@@ -1,16 +1,90 @@
-import {dishes} from "../../tempData.ts";
-import {useState} from "react";
+import {dishes,drinks} from "../../tempData.ts";
+import {getRandomItems} from "../../utils/functions.ts";
+import {useEffect, useState} from "react";
 
- const NewOrder = () => {
+interface Props{
+    numberOfPeople: number
+}
 
-     const [number,setNumber] = useState<Number>(0)
+interface Item {
+    name: string
+    price: number
+    cat: string
+}
 
-     setNumber(Math.floor(Math.random() * 11))
+const NewOrder = (props:Props) => {
+
+    const [mainCourses, setMainCourses] = useState<Item[]>([])
+    const [desserts, setDesserts] = useState<Item[]>([])
+    const [coldDrinks, setColdDrinks] = useState<Item[]>([])
+    const [warmDrinks, setWarmDrinks] = useState<Item[]>([])
+
+    useEffect(() => {
+        const mainCoursesArray:Item[] = []
+        const dessertsArray:Item[] = []
+        const coldDrinksArray:Item[] = []
+        const warmDrinksArray:Item[] = []
+
+        dishes.map((dish) => {
+            if(dish.cat !=='Desserts'){
+                mainCoursesArray.push(dish)
+            } else {
+                dessertsArray.push(dish)
+            }
+        })
+
+        drinks.map((drink) => {
+            if(drink.cat !== "Boissons chaudes"){
+                coldDrinksArray.push(drink)
+            } else {
+                warmDrinksArray.push(drink)
+            }
+        })
+
+        setMainCourses(getRandomItems(mainCoursesArray,props.numberOfPeople))
+        setDesserts(getRandomItems(dessertsArray,props.numberOfPeople))
+        setColdDrinks(getRandomItems(coldDrinksArray,props.numberOfPeople))
+        setWarmDrinks(getRandomItems(warmDrinksArray,props.numberOfPeople))
+
+    },[props.numberOfPeople])
+
 
     return (
-        <div>
-
-        </div>
+        <section className={"w-full h-auto flex flex-col items-center justify-between pt-2"}>
+            <h1 className={"text-center text-white text-2xl font-inter"}>Commande pour {props.numberOfPeople} personnes</h1>
+            <div className={"w-full"}>
+                <h2 className={"text-lg text-center font-inter underline"}>Plats</h2>
+                <ul>
+                    {mainCourses.map((course,index) => (
+                        <li className={"text-base text-white text-center font-inter"} key={index}>{course.name} - {course.price} €</li>
+                    ))}
+                </ul>
+            </div>
+            <div className={"w-full"}>
+                <h2 className={"text-lg text-center font-inter underline"}>Desserts</h2>
+                <ul>
+                    {desserts.map((course, index) => (
+                        <li className={"text-base text-white text-center font-inter"}
+                            key={index}>{course.name} - {course.price} €</li>
+                    ))}
+                </ul>
+            </div>
+            <div className={"w-full"}>
+                <h2 className={"text-lg text-center font-inter underline"}>Boissons</h2>
+                <ul>
+                    {coldDrinks.map((course, index) => (
+                        <li className={"text-base text-center text-white font-inter"}
+                            key={index}>{course.name} - {course.price} €</li>
+                    ))}
+                </ul>
+                <ul>
+                    {warmDrinks.map((course, index) => (
+                        <li className={"text-lg text-center text-white font-inter"}
+                            key={index}>{course.name} - {course.price} €</li>
+                    ))}
+                </ul>
+            </div>
+        </section>
     );
 };
 export default NewOrder;
--- restaurant-manager-ui/src/pages/restaurant/Restaurant.tsx
+++ restaurant-manager-ui/src/pages/restaurant/Restaurant.tsx
@@ -1,21 +1,25 @@
 import {useNavigate} from "react-router-dom"
 import {useEffect, useState} from "react";
 import TimeDisplay from "../../components/timeDisplay/TimeDisplay.tsx";
-
+import NewOrder from "../../components/newOrder/NewOrder.tsx";
 
 
 const Restaurant = () => {
 
     const navigate = useNavigate()
 
-    const [username,setUsername] = useState("");
+    const [username, setUsername] = useState<string>("");
+    const [showNewOrder, setShowNewOrder] = useState<boolean>(false);
+    const [numberOfPeople,setNumberOfPeople] = useState<number>(0)
+
+
 
     useEffect(() => {
         const storedUsername = localStorage.getItem("username");
-        if(storedUsername){
+        if (storedUsername) {
             setUsername(storedUsername);
         }
-    },[])
+    }, [])
 
     const handleLogout = () => {
         localStorage.removeItem("username")
@@ -23,7 +27,8 @@ const Restaurant = () => {
     }
 
     const handleNewOrder = () => {
-
+        setNumberOfPeople(Math.floor(Math.random() * 11)+1)
+        setShowNewOrder(true);
     }
 
     return (
@@ -55,25 +60,30 @@ const Restaurant = () => {
                                 <path
                                     d="M2 4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm10 1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1zm4 3a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8"/>
                             </svg>
-                            {/*<Link to={'../connexion'}>*/}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                     className="bi bi-box-arrow-left cursor-pointer" onClick={handleLogout} viewBox="0 0 16 16">
-                                    <path fillRule="evenodd"
-                                          d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
-                                    <path fillRule="evenodd"
-                                          d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
-                                </svg>
-                            {/*</Link>*/}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                 className="bi bi-box-arrow-left cursor-pointer" onClick={handleLogout}
+                                 viewBox="0 0 16 16">
+                                <path fillRule="evenodd"
+                                      d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+                                <path fillRule="evenodd"
+                                      d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                            </svg>
                         </div>
                     </div>
-                    <TimeDisplay />
-                    <h2 className={"text-center text-white text-2xl font-inter italic"}>Bienvenue {username}</h2>
-                    <button
-                        className={"w-[50%] mx-auto h-12 px-6 py-auto bg-[#013220] hover:bg-[#6B8E23] text-white text-base font-inter rounded-md cursor-pointer"}
-                        onClick={handleNewOrder}
-                    >
-                        Nouvelle commande
-                    </button>
+                    {showNewOrder === false ? (
+                        <>
+                            <TimeDisplay />
+                            <h2 className={"text-center text-white text-2xl font-inter italic"}>Bienvenue {username}</h2>
+                            <button
+                                className={"w-[50%] mx-auto h-12 px-6 py-auto bg-[#013220] hover:bg-[#6B8E23] text-white text-base font-inter rounded-md cursor-pointer"}
+                                onClick={handleNewOrder}
+                            >
+                                Nouvelle commande
+                            </button>
+                        </>
+                    ) : (
+                        <NewOrder numberOfPeople={numberOfPeople}/>
+                    )}
                     <div className={"w-auto flex items-center justify-end gap-3 px-4"}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor"
                              className="bi bi-volume-off" viewBox="0 0 16 16">
