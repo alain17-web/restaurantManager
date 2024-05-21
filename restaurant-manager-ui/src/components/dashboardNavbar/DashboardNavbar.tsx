3a65ba78269c740aafbd8eb29b57c22e203a5115
alain17-web <web2020.alain@gmail.com>
2024-05-21 22:38:41 +0200

    ADD NavLink Dashboard to DashboardSidebar

--- restaurant-manager-ui/src/components/dashboardNavbar/DashboardNavbar.tsx
+++ restaurant-manager-ui/src/components/dashboardNavbar/DashboardNavbar.tsx
@@ -20,20 +20,11 @@ const DashboardNavbar = () => {
     }
 
     return (
-        <div className={"h-12 flex items-center text-[#555] text-base bg-[#ADD8E6] border border-[#D3D3D3]"}>
+        <div className={"h-16 flex items-center text-[#555] text-base bg-[#ADD8E6] border border-[#D3D3D3]"}>
             <div className={"w-full flex items-center justify-between p-5"}>
                 <div className={"flex items-center"}>
                     <div className={"flex items-center mr-5 pr-1 relative"}>
-
-                        <Link className={"font-inter decoration-0 italic pl-1"}>Bienvenue {username}</Link>
-                    </div>
-                    <div className={"flex items-center mr-5 pr-1 relative"}>
-                        <img src={"./img/restaurant-waiter.svg"} width={20} height={20}/>
-                        <Link to={'/restaurant'} className={'text-[#555] decoration-0 cursor-pointer pl-1'}>Salle</Link>
-                    </div>
-                    <div className={"flex items-center mr-5 pr-1 relative"}>
-                        <img src={"./img/chef.svg"} width={20} height={20}/>
-                        <Link to={'/kitchen'} className={'text-[#555] decoration-0 cursor-pointer pl-1'}>Cuisine</Link>
+                        <Link className={"text-lg font-inter decoration-0 italic pl-1"}>Bienvenue {username}</Link>
                     </div>
                     <div className={"flex items-center mr-5 pr-1 relative"}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
@@ -44,8 +35,17 @@ const DashboardNavbar = () => {
                             <path fillRule="evenodd"
                                   d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
                         </svg>
-                        <Link className={'text-[#555] decoration-0 cursor-pointer pl-1'}>Logout</Link>
+                        <Link className={'text-[#555] decoration-0 cursor-pointer pl-1 font-semibold'}>Logout</Link>
                     </div>
+                    <div className={"flex items-center mr-5 pr-1 relative"}>
+                        <img src={"./img/restaurant-waiter.svg"} width={20} height={20}/>
+                        <Link to={'/restaurant'} className={'text-[#555] decoration-0 cursor-pointer pl-1'}>Salle</Link>
+                    </div>
+                    <div className={"flex items-center mr-5 pr-1 relative"}>
+                        <img src={"./img/chef.svg"} width={20} height={20}/>
+                        <Link to={'/kitchen'} className={'text-[#555] decoration-0 cursor-pointer pl-1'}>Cuisine</Link>
+                    </div>
+
                 </div>
             </div>
         </div>
--- restaurant-manager-ui/src/components/dashboardSidebar/DashboardSidebar.tsx
+++ restaurant-manager-ui/src/components/dashboardSidebar/DashboardSidebar.tsx
@@ -1,9 +1,40 @@
+import {Link, NavLink, useLocation} from "react-router-dom"
+import classNames from 'classnames'
 
 const DashboardSidebar = () => {
-    return (
-        <div className={"flex-1 min-h-screen border-r border-[#D3D3D3]"}>
-            <div className={"h-12 flex items-center justify-center bg-[#ADD8E6]"}>
 
+    const location = useLocation()
+
+    return (
+        <div className={"flex-1 min-h-screen border-r-[0.5px] border-[#D3D3D3]"}>
+            <div className={"h-16 flex items-center justify-center bg-[#ADD8E6]"}>
+                <img src={"./img/logo.png"} alt="logo" width="48px" height="48px"/>
+                <Link className={"font-inter text-[#013220] decoration-0 italic font-semibold"}>La Branche
+                    d'Olivier</Link>
+            </div>
+            <hr className={'h-0 border-[O.5px] border-[#D3D3D3]'}/>
+            <div className={'pl-[10px]'}>
+                <ul className={"m-0 p-0 list-none"}>
+                    <p className={"text-base text-[#999] mt-[15px] mb-[15px] font-inter font-bold"}>GENERAL</p>
+                    <NavLink
+                        to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#7451f8"
+                                 className="bi bi-bar-chart-fill mr-[10px]" viewBox="0 0 16 16">
+                                <path
+                                    d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z"/>
+                            </svg>
+                            Dashboard
+                        </li>
+                    </NavLink>
+                </ul>
             </div>
         </div>
     );
