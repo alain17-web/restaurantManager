2bd9ce49a00807e23fe1a8112b7e6a1af6c44136
alain17-web <web2020.alain@gmail.com>
2024-05-22 14:45:13 +0200

    IMPROV DashboardSidebar

--- restaurant-manager-ui/src/components/dashboardSidebar/DashboardSidebar.tsx
+++ restaurant-manager-ui/src/components/dashboardSidebar/DashboardSidebar.tsx
@@ -1,9 +1,16 @@
-import {Link, NavLink, useLocation} from "react-router-dom"
+import {Link, NavLink, useLocation, useNavigate} from "react-router-dom"
 import classNames from 'classnames'
 
 const DashboardSidebar = () => {
 
     const location = useLocation()
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("username")
+        navigate("/connexion");
+    }
 
     return (
         <div className={"flex-1 min-h-screen border-r-[0.5px] border-[#D3D3D3]"}>
@@ -25,15 +32,192 @@ const DashboardSidebar = () => {
                             })
                         }
                     >
-                        <li>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#7451f8"
-                                 className="bi bi-bar-chart-fill mr-[10px]" viewBox="0 0 16 16">
-                                <path
-                                    d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z"/>
-                            </svg>
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/dashboard.svg'} width={25} height={25} className={'mr-[10px]'}/>
                             Dashboard
                         </li>
                     </NavLink>
+                    <p className={"text-base text-[#999] mt-[15px] mb-[15px] font-inter font-bold"}>STAFF</p>
+                    <NavLink
+                        to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/people.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Employés Actifs
+                        </li>
+                    </NavLink>
+                    <NavLink
+                        to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/unfollow.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Inactifs
+                        </li>
+                    </NavLink>
+                    <p className={"text-base text-[#999] mt-[15px] mb-[15px] font-inter font-bold"}>MENU</p>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/dish.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Plats
+                        </li>
+                    </NavLink>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/glass.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Boissons
+                        </li>
+                    </NavLink>
+                    <p className={"text-base text-[#999] mt-[15px] mb-[15px] font-inter font-bold"}>COMMANDES</p>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/clientOrder.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Commandes Clients
+                        </li>
+                    </NavLink>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/purchase-order.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Réappros Fournisseurs
+                        </li>
+                    </NavLink>
+                    <p className={"text-base text-[#999] mt-[15px] mb-[15px] font-inter font-bold"}>STOCK</p>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/freezer.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Stock Frais
+                        </li>
+                    </NavLink>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/bottles.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Stock Boissons
+                        </li>
+                    </NavLink>
+                    <p className={"text-base text-[#999] mt-[15px] mb-[15px] font-inter font-bold"}>RESERVATIONS</p>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/booking.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Confirmées
+                        </li>
+                    </NavLink>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/cancel.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Annulées - noShow
+                        </li>
+                    </NavLink>
+                    <p className={"text-base text-[#999] mt-[15px] mb-[15px] font-inter font-bold"}>FINANCES</p>
+                    <NavLink
+                        //to={location.pathname !== "/dashboard" ? "/dashboard" : "#"}
+                        to={'#'}
+                        className={({isActive}: { isActive: boolean }) =>
+                            classNames('no-underline', {
+                                'text-gray-500': !isActive,
+                                'text-purple-600': isActive,
+                            })
+                        }
+                    >
+                        <li className={"flex items-center p-1.5 cursor-pointer"}>
+                            <img src={'./img/euro.svg'} width={25} height={25} className={'mr-[10px]'}/>
+                            Comptabilité
+                        </li>
+                    </NavLink>
+                    <li className={'flex items-center p-1.5 cursor-pointer mt-10'} onClick={handleLogout}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                             className="bi bi-box-arrow-left mr-[10px] cursor-pointer"
+                             viewBox="0 0 16 16">
+                            <path fillRule="evenodd"
+                                  d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+                            <path fillRule="evenodd"
+                                  d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                        </svg>
+                        <Link
+                            className={'text-[#555] hover:text-red-400 text-lg decoration-0 cursor-pointer pl-1 font-semibold'}>Déconnexion</Link>
+                    </li>
                 </ul>
             </div>
         </div>
