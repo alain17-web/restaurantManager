c245dc23566244aa6a91e375d457513a04cc174d
alain17-web <web2020.alain@gmail.com>
2024-05-22 17:09:29 +0200

    ADD Widget to Dashboard

--- restaurant-manager-ui/src/pages/dashboard/Dashboard.tsx
+++ restaurant-manager-ui/src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,20 @@
 //import {Link} from "react-router-dom"
 import DashboardSidebar from "../../components/dashboardSidebar/DashboardSidebar.tsx";
 import DashboardNavbar from "../../components/dashboardNavbar/DashboardNavbar.tsx";
+import Widget from "../../components/widget/Widget.tsx";
 
 const Dashboard = () => {
     return (
         <section className={"flex"}>
             <DashboardSidebar/>
-            {/*<Link to={"/connexion"}>Back to Login</Link>*/}
             <div className={"flex-[6]"}>
                 <DashboardNavbar/>
+
+                <div className={"flex p-5 gap-5"}>
+                <Widget type={"commandes"}/>
+                <Widget type={"reappros"}/>
+                <Widget type={"reservations"}/>
+                </div>
             </div>
         </section>
     );
