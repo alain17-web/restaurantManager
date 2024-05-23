4f17cd2c0b203c3a2f5bae4d4d6b5f402b46bd65
alain17-web <web2020.alain@gmail.com>
2024-05-23 11:54:06 +0200

    CORR MenuButton

--- restaurant-manager-ui/src/pages/home/Home.tsx
+++ restaurant-manager-ui/src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom"
 import BookButton from "../../components/bookButton/BookButton.tsx";
+import MenuButton from "../../components/menuButton/MenuButton.tsx";
 
 export const Home = () => {
     return (
@@ -9,9 +9,7 @@ export const Home = () => {
                 <BookButton/>
             </div>
             <h1 className={"font-inter text-6xl text-black text-center font-semibold italic pt-[20%]"}>La Branche d'Olivier</h1>
-            <Link to={"/menu"}>
-                <button className={"w-[10vw] m-auto h-12 flex items-center justify-center rounded-2xl px-12 py-auto mt-4 text-white bg-[#6B8E23] hover:text-[#013220] hover:bg-amber-50 hover: border-4 hover:border-[#013220]"}>Menu</button>
-            </Link>
+            <MenuButton/>
         </section>
 
     );
