deed3e584dd6a29e1d753be5892f4d6f2b03a124
alain17-web <web2020.alain@gmail.com>
2024-06-01 17:02:15 +0200

    ADD DateDisplay on Dashboard

---
+++ restaurant-manager-ui/src/components/dateDisplay/DateDisplay.tsx
@@ -0,0 +1,21 @@
+
+
+const CurrentDate = () => {
+    const currentDate = new Date();
+
+
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const year = currentDate.getFullYear();
+
+
+    const formattedDate = `${day}/${month}/${year}`;
+
+    return (
+        <div className={'w-full h-auto'}>
+            <p className={"text-lg text-center font-inter"}> {formattedDate}</p>
+        </div>
+    );
+};
+
+export default CurrentDate;
--- restaurant-manager-ui/src/pages/dashboard/Dashboard.tsx
+++ restaurant-manager-ui/src/pages/dashboard/Dashboard.tsx
@@ -2,19 +2,29 @@
 import DashboardSidebar from "../../components/dashboardSidebar/DashboardSidebar.tsx";
 import DashboardNavbar from "../../components/dashboardNavbar/DashboardNavbar.tsx";
 import Widget from "../../components/widget/Widget.tsx";
+import TimeDisplay from "../../components/timeDisplay/TimeDisplay.tsx";
+import {useState} from "react";
+import DateDisplay from "../../components/dateDisplay/DateDisplay.tsx";
 
 const Dashboard = () => {
+
     return (
-        <section className={"flex"}>
+        <section className={"h-auto flex"}>
             <DashboardSidebar/>
             <div className={"flex-[6]"}>
                 <DashboardNavbar/>
 
                 <div className={"flex p-5 gap-5"}>
-                <Widget type={"commandes"}/>
-                <Widget type={"reappros"}/>
-                <Widget type={"reservations"}/>
+                    <Widget type={"commandes"}/>
+                    <Widget type={"reappros"}/>
+                    <Widget type={"reservations"}/>
                 </div>
+                <div className={"w-[10%] h-auto mx-auto mt-16 bg-[#008080] p-2 rounded-xl"}>
+                    <TimeDisplay/>
+                </div>
+                <DateDisplay/>
+
+
             </div>
         </section>
     );
