e079886f1a17659b1a315f1b3958e8e7af7c6a1f
alain17-web <web2020.alain@gmail.com>
2024-09-12 12:28:23 +0200

    ADD comments to RoleOption

--- restaurant-manager-ui/src/components/dashboardComponents/roleOptions/RoleOption.tsx
+++ restaurant-manager-ui/src/components/dashboardComponents/roleOptions/RoleOption.tsx
@@ -6,20 +6,26 @@ const RoleOption = () => {
 
     const [roles, setRoles] = useState<Role[]>([]);
 
+    // useEffect hook to fetch roles when the component mounts
     useEffect(() => {
         const getRoles = async () => {
             try {
+                // Sending a GET request to retrieve the list of roles
                 const res = await axiosInstance.get('/roles/')
+
+                // Updating the state with the fetched roles data
                 setRoles(res.data)
             } catch (error) {
                 console.error('Error in getRoles', error);
             }
         }
         getRoles()
-    }, []);
+    }, []);  // Empty dependency array ensures this effect runs only once, on component mount
+
     return (
         <>
             <option>Choisir un rôle</option>
+            {/* Mapping over the fetched roles and rendering an option for each role */}
             {roles.map((role) => (
                 <option key={role.id} value={role.id}>{role.role_name}</option>
             ))}
