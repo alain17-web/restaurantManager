d34a6c3f7d8f0f52768a9dec32d1e3fc85f89b52
alain17-web <web2020.alain@gmail.com>
2024-09-12 11:00:27 +0200

    ADD comments to CategoryOptions

--- restaurant-manager-ui/src/components/dashboardComponents/categoryOptions/CategoryOptions.tsx
+++ restaurant-manager-ui/src/components/dashboardComponents/categoryOptions/CategoryOptions.tsx
@@ -11,6 +11,7 @@ interface Props {
     const [categories, setCategories] = useState<Category[]>([]);
 
     useEffect(() => {
+        // Function to fetch categories from the API
          const getCategories = async () => {
              try {
                  const res = await axiosInstance.get('/categories/')
@@ -20,12 +21,13 @@ interface Props {
              }
          }
          getCategories()
-     }, []);
+     }, []);// Empty dependency array ensures this effect runs only once, when the component mounts
 
     return (
         <>
             <option>Choisir une catégorie</option>
             {categories.map((category) =>
+                // Check if the category type matches the isDish flag (food or beverage)
                 category.type === (isDish ? "food" : "beverage") ? (
                     <option key={category.id} value={category.id}>{category.cat_name}</option>) : null
             )}
