b78eeb4ef0fa5206ba5688c7d7a73501dfd8cb1d
alain17-web <web2020.alain@gmail.com>
2024-09-18 18:08:25 +0200

    IMPROV Home responsive

--- restaurant-manager-ui/src/pages/publicPages/home/Home.tsx
+++ restaurant-manager-ui/src/pages/publicPages/home/Home.tsx
@@ -1,8 +1,26 @@
 import BookButton from "../../../components/generalComponents/bookButton/BookButton.tsx";
 import MenuButton from "../../../components/generalComponents/menuButton/MenuButton.tsx";
 import GuestLoginButton from "../../../components/generalComponents/guestLoginButton/GuestLoginButton.tsx";
+import {useState,useEffect} from "react";
 
 export const Home = () => {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    // Use useEffect to listen to window resize event
+    useEffect(() => {
+        // Function to update the window width state
+        const handleResize = () => setWindowWidth(window.innerWidth);
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const isLargeScreen = windowWidth >= 1280;
+
     return (
 
         <section
@@ -13,7 +31,8 @@ export const Home = () => {
             <h1 className={"font-inter text-6xl text-white text-center font-semibold italic pt-[10%] mb-12"}>La Branche
                 d'Olivier</h1>
             <MenuButton/>
-            <GuestLoginButton/>
+            {/* Component rendered if the screen width is 1280px or larger */}
+            {isLargeScreen && <GuestLoginButton/>}
         </section>
     );
 };
